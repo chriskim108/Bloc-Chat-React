09400d8c7f2b2be5c4b96d9ca93fd0e10ca393dd
Christopher Kim <Chriskimdevelop@gmail.com>
2018-05-07 13:39:37 -0400

    Chat Room Checkup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import RoomList from "./components/RoomList";
 import './App.css';
+import * as firebase from 'firebase';
 
 class App extends Component {
   render() {
@@ -13,6 +15,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <RoomList />
       </div>
     );
   }

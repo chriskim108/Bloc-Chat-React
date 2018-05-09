0e31c9793558a5dc05d5d71c01aa669601e3df23
Christopher Kim <Chriskimdevelop@gmail.com>
2018-05-09 16:56:04 -0400

    Create Chat Rooms: Completed 1

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -19,6 +19,17 @@ class RoomList extends Component{
             this.setState({ rooms: this.state.rooms.concat( room ) })
         })
     }
+
+    createRoom(e){
+        e.preventDefault();
+        // Retriving the values from the input tag
+        let newRoom = this.refs.nameOfRoom.value; 
+        
+        // Pushing it to FireBase
+        this.roomsRef.push({ 
+            name: newRoom
+        });  
+    }
     
     render(){
         return(
@@ -33,6 +44,23 @@ class RoomList extends Component{
                         </div>
                     ) 
                 }
+
+                <form onSubmit={ this.createRoom.bind(this) } >
+                    <label className="newRoomLabel">Create a new room</label>
+                    
+                    <br/>
+
+                    <input 
+                        type="text" 
+                        ref="nameOfRoom" 
+                        placeholder="Create a new room"
+                        className="newRoomInput"/>
+
+                    <input 
+                        type="submit" 
+                        value="Create Room" 
+                        className="newRoomSubmit"/>
+                </form>
 
             </div>
         )
